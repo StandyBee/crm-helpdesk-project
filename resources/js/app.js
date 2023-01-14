68ed6740c62b0cfbd73c9817d62285e2e604b342
fisak <afisak94@gmail.com>
2023-01-14 08:35:42 +0300

    add vue component

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,5 @@
-import './bootstrap';
+import { createApp } from "vue";
+//import Welcome from "./Welcome.vue";
+import Home from './components/Home.vue';
+
+createApp(Home).mount('#app');
---
+++ resources/js/components/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+      <h1>CRM HELPDESK</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "Home"
+  }
+  </script>
+  
