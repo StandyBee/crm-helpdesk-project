897b98a7010d5b181f4c84989f2876fda4ac691a
fisak <afisak94@gmail.com>
2023-01-15 07:42:32 +0300

    modify vue component

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -1,12 +1,42 @@
 <template>
+  <div>
+      <h1>{{ title }}</h1>
+    <input type="text" v-model="title">
+    <ul>
+      <li v-for="user in users">{{ user }}</li>
+    </ul>
+    <input type="text" v-model="name">
+    <button type="button" @click="addUser">Add</button>
     <div>
-      <h1>CRM HELPDESK</h1>
+      <h2>{{ user.name }}</h2>
+      <h2>{{ user.last }}</h2>
     </div>
-  </template>
+  </div>
+</template>
   
-  <script>
-  export default {
-    name: "Home"
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {
+      title: "Crm Helppdesk",
+      users: ['Anton', 'Dima', 'Roma'],
+      user: {
+        name: 'first',
+        last: 'last',
+      },
+      name: null,
+    }
+  },
+  methods: {
+    addUser() {
+      this.users.push(this.name);
+      this.clearName();
+    },
+    clearName() {
+      this.name = null;
+    }
   }
-  </script>
+}
+</script>
   
